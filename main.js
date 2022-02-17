6f19ca7087d2b8ddf15293f5684b27f6a40a9e22
Dongho Yang <helium050530@naver.com>
2022-02-17 12:13:13 +0900

    asdf

---
+++ main.js
@@ -0,0 +1,28 @@
+var gameData = {
+  money: 0,
+  moneyps: 1,
+  upcost:10000
+}
+function getmoney(){
+  gameData.money+=gameData.moneyps
+  document.getElementById("money").innerHTML = gameData.money
+}
+function upgrade() {
+  if (gameData.money >= gameData.upcost) {
+    gameData.money -= gameData.upcost
+    gameData.moneyps *= 2
+    gameData.upcost *= 2.2
+    document.getElementById("money").innerHTML = gameData.gold + " Gold Mined"
+    document.getElementById("upgradecost").innerHTML = "upgrade Cost: " + gameData.upcost
+  }
+}
+var mainloop = window.setInterval(function() {
+  getmoney()
+},100)
+var saveloop = window.setInterval(function() {
+  localStorage.setItem("save", JSON.stringify(gameData))
+}, 30000)
+var savegame = JSON.parse(localStorage.getItem("save"))
+if (savegame !== null) {
+gameData = savegame
+}
